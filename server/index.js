1877fe7c1f236f1e750767762073bdc85092c0cd
Yui Davidson <yuidavidson@gmail.com>
2020-08-19 23:21:34 -0700

    Server connect without middleware:

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 // require express and also app = express()
 const express = require('express');
+// const cors = require('cors');
+const axios = require('axios');
 const app = express();
 const url = require('url');
 
 // TODO: write own middleware
-const httpProxy = require('http-proxy');
-const apiProxy = httpProxy.createProxyServer();
+// const httpProxy = require('http-proxy');
+// const apiProxy = httpProxy.createProxyServer();
 // probably need endpoints for all services
 
 const serverOne = 'http://localhost:3001';
@@ -26,28 +28,111 @@ app.use(express.json());
 //   res.send('hello world');
 // });
 
-app.all('/reviews/*', function(req, res) {
-  const queryObject = url.parse(req.url, true).query;
-  console.log(queryObject);
-  console.log('redirecting to Server1');
-  apiProxy.web(req, res, {target: serverOne});
+// the proxy sends a request to the target
+// receives a response from the target
+// send the response back to the client
+
+// app.all('/listing/*', function(req, res) {
+//   const queryObject = url.parse(req.url, true).query;
+//   console.log(queryObject);
+//   console.log('redirecting to Server2');
+//   apiProxy.web(req, res, {target: serverOne});
+// });
+
+// app.all('/reviews/*', function(req, res) {
+//   const queryObject = url.parse(req.url, true).query;
+//   console.log(queryObject);
+//   console.log('redirecting to Server1');
+//   apiProxy.web(req, res, {target: serverTwo});
+// });
+
+// app.all('/test1/*', function(req, res) {
+//   const queryObject = url.parse(req.url, true).query;
+//   console.log(queryObject);
+//   console.log('redirecting to Server3');
+//   apiProxy.web(req, res, {target: serverThree});
+// });
+
+app.get('/listing*', (req, res) => {
+  const sendData = (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  };
+  const getData = (callback) => {
+    axios.get(serverOne + req.url)
+      .then((res) => {
+        callback(null, res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  getData(sendData);
 });
 
-app.all('/listing/*', function(req, res) {
-  const queryObject = url.parse(req.url, true).query;
-  console.log(queryObject);
-  console.log('redirecting to Server2');
-  apiProxy.web(req, res, {target: serverTwo});
+app.get('/reviews*', (req, res) => {
+  const sendData = (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  };
+  const getData = (callback) => {
+    axios.get(serverTwo + req.url)
+      .then((res) => {
+        callback(null, res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  getData(sendData);
 });
 
-app.all('/test1/*', function(req, res) {
-  const queryObject = url.parse(req.url, true).query;
-  console.log(queryObject);
-  console.log('redirecting to Server3');
-  apiProxy.web(req, res, {target: serverThree});
+app.get('/test1*', (req, res) => {
+  const sendData = (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  };
+  const getData = (callback) => {
+    axios.get(serverThree + req.url)
+      .then((res) => {
+        callback(null, res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  getData(sendData);
+
+  // axios.get(serverThree + req.url)
+  //   .then((req, res) => {
+  //     res.status(200).send(res.data);
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
 });
+
+// app.route('http://localhost:3003/*')
+//   .all(function (req, res, next) {
+//     // runs for all HTTP verbs first
+//     // think of it as route specific middleware!
+//   })
+//   .get(function (req, res, next) {
+//     res.json({});
+//   });
 
 // listen on port of preference(?) if running locally, i'll probably need different ports for different services or perhaps just different paths?
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(`listening at http://localhost:${PORT}`);
 });
